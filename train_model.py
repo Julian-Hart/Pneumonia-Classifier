936689d2d5c804e764be0f412e08e23a83935ce8
Julian Hart <julianja.hart@gmail.com>
2025-03-30 13:13:34 -0400

    cal

---
+++ train_model.py
@@ -0,0 +1,10 @@
+import os
+
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from torchvision import models, transforms
+from sklearn.metrics import accuracy_score
